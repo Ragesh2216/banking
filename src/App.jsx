c91e0e6075a0dafdfd0252d11545d076c67c4f59
rahul <murahul76@gmail.com>
2025-12-07 12:24:20 +0530

    ghj

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import Footer from "./components/Footer";
+import { HashRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Scrooltop from "./components/Scrooltop";
 import './App.css';
 
 function App({ element }) {
   return (
-    <>
+    <><HashRouter>
+          
+       
     <Scrooltop/>
       <Navbar />
       {element}
       <Footer />
-     
+      </HashRouter>
     </>
   );
 }
